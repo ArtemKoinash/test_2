a96fe99afd0b93bd5d926ca87a1dfbcd69be3ede
ArtemKoinash <artemkoinashhh@gmail.com>
2019-09-25 22:40:10 +0300

    homework

---
+++ Lesson2_HW.py
@@ -0,0 +1,18 @@
+#task 1.1
+q = "asdfghjk"
+str.isnumeric(q)
+#task 1.2
+w = "How are you filling...., darling?"
+str.count(w, ' ')
+#task 1.3
+str.count(w, '.')
+#task 1.4
+e = "Homework"
+str.center(e, 100)
+#task 1.5
+r = "hey dude, whats up"
+str.title(r)
+
+#task 2.1
+math.hypot(179, 971)
+
